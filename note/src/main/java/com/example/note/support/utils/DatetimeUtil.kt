a8e7a915b903c7245d95c5874107ede7d53c657d
Long Do <Longdt57@gmail.com>
2025-01-23 18:02:01 +0900

    Fix DatetimeUtilTest

--- note/src/main/java/com/example/note/support/utils/DatetimeUtil.kt
+++ note/src/main/java/com/example/note/support/utils/DatetimeUtil.kt
@@ -3,13 +3,18 @@ package com.example.note.support.utils
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import java.util.TimeZone
 
 object DatetimeUtil {
 
     private const val NOTE_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss"
 
-    fun formatDateTime(timestamp: Long, locale: Locale = Locale.getDefault()): String {
-        val format = SimpleDateFormat(NOTE_DATE_TIME_FORMAT, locale)
+    fun formatDateTime(
+        timestamp: Long,
+        timeZone: TimeZone = TimeZone.getDefault()
+    ): String {
+        val format = SimpleDateFormat(NOTE_DATE_TIME_FORMAT, Locale.getDefault())
+        format.timeZone = timeZone
         return format.format(Date(timestamp))
     }
 }
--- note/src/test/java/com/example/note/support/util/DateTimeUtilTest.kt
+++ note/src/test/java/com/example/note/support/util/DateTimeUtilTest.kt
@@ -3,16 +3,16 @@ package com.example.note.support.util
 import com.example.note.support.utils.DatetimeUtil
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import java.util.Locale
+import java.util.TimeZone
 
 class DatetimeUtilTest {
 
     @Test
     fun testFormatDateTime() {
         val timestamp = 1705944600000
-        val expectedDate = "2024-01-23 02:30:00"
+        val expectedDate = "2024-01-22 17:30:00"
 
-        val result = DatetimeUtil.formatDateTime(timestamp, locale = Locale.US)
+        val result = DatetimeUtil.formatDateTime(timestamp, TimeZone.getTimeZone("GMT"))
         assertEquals(expectedDate, result)
     }
 }
